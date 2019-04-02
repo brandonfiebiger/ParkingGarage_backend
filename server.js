876dc8e2f221f35f6dd4cb3c06a952e1a9736734
Brandon Fiebiger <brandonfiebiger@gmail.com>
2019-04-01 22:56:26 -0600

    Add comments for the endpoints.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const database = require('knex')(configuration);
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+//Get all of the parking spaces in the database
 app.get('/api/v1/spaces', (request, response) => {
   database('spaces').select()
     .then((spaces) => {
@@ -16,6 +17,7 @@ app.get('/api/v1/spaces', (request, response) => {
     });
 });
 
+//Get all of the vehicles in the database
 app.get('/api/v1/vehicles', (request, response) => {
   database('vehicles').select()
     .then((vehicles) => {
@@ -26,6 +28,7 @@ app.get('/api/v1/vehicles', (request, response) => {
     });
 });
 
+//Add a new vehicle object to the database
 app.post('/api/v1/vehicles', (request, response) => {
   const vehicle = request.body;
 
@@ -46,7 +49,7 @@ app.post('/api/v1/vehicles', (request, response) => {
     });
 });
 
-
+//Update space with a newly parked vehicle_id
 app.patch('/api/v1/spaces/:id', (request, response) => {
 
   const space = request.body;
@@ -67,6 +70,8 @@ app.patch('/api/v1/spaces/:id', (request, response) => {
   })
 })
 
+
+//Remove a vehicle from a parking space by setting the vehicle id to null
 app.put('/api/v1/spaces/:id', (request, response) => {
 
   const space = request.body;
@@ -87,13 +92,14 @@ app.put('/api/v1/spaces/:id', (request, response) => {
   });
 });
 
+//delete vehicle from database
 app.delete('/api/v1/vehicles/:id', (request, response) => {
   database('vehicles')
   .select()
   .where('id', request.params.id)
   .del()
   .then(() => {
-    return response.sendStatus(202)
+    return response.status(202).json('success');
   })
   .catch(error => {
     return response.status(500).send({ error });
@@ -103,3 +109,5 @@ app.delete('/api/v1/vehicles/:id', (request, response) => {
 app.listen(5000, () => {
   console.log('app is listening on port 5000');
 });
+
+module.exports = app;
